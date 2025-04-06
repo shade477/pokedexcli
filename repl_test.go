dc2c5b2ac7e6b306c9f5fef4b58782a765742780
shade477 <sagardeyof477@gmail.com>
2025-04-06 21:01:57 +0530

    Added test cases for cleanInput

---
+++ repl_test.go
@@ -0,0 +1,52 @@
+package main
+
+import "testing"
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct{
+		input		string
+		expected	[]string
+	}{
+		{
+			input: "    hello world    ",
+			expected: []string{"hello", "world"},
+		},
+		// {
+		// 	input: "    hello    world    ",
+		// 	expected: []string{"hello", "world"},
+		// },
+		{
+			input: "    hello world     ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input: "    hello world",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input: "hello world    ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input: "hello-world",
+			expected: []string{"hello-world"},
+		},
+	}
+
+	for _,c := range cases {
+		actual := cleanInput(c.input)
+
+		if len(actual) != len(c.expected) {
+			t.Errorf("cleanInput(%q) returned %d words, expected %d", c.input, len(actual), len(c.expected))
+		}
+
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+
+			if word != expectedWord {
+				t.Errorf("cleanInput(%q)[%d] = %q, expected %q", c.input, i, word, expectedWord)
+			}
+		}
+	}
+}
