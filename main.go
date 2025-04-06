ff3209f5a13db1b5f16fa128ea7470f6c73f3c8d
shade477 <sagardeyof477@gmail.com>
2025-04-06 21:01:42 +0530

    added cleanInput function

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-	fmt.Print("Hello World")
+	fmt.Print("Hello, World!")
+}
+
+func cleanInput(text string) []string {
+	res := strings.Split(strings.Trim(text, " "), " ")
+	return res
 }
